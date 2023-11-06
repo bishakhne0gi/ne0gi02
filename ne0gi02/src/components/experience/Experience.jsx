3de7e1422e73e1409eb12385e3bc0b00023c6841
bishakhne0gi <bneogi102002@gmail.com>
2023-11-06 16:18:10 +0530

    feat: experience

--- ne0gi02/src/components/experience/Experience.jsx
+++ ne0gi02/src/components/experience/Experience.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import './Experience.css'
-import AOT from '../../assets/aot.png'
-import DBB from '../../assets/dbb.png'
-import ICSE from '../../assets/icse.png'
-import ISC from '../../assets/isc.png'
-import CGPA from '../../assets/cgpa.png'
 
+import DEV from '../../assets/dev.png'
 
 const Experience = () => {
     return (
@@ -19,7 +15,7 @@ const Experience = () => {
 
                     <div className="text it1" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="500">Upcoming Software Engineer</div>
                     <hr className="hrz it2" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="500" />
-                    <img className="ig it3" src={DBB} data-aos="flip-up" data-aos-easing="linear" data-aos-duration="500"></img>
+                    <img className="ig it3" src={DEV} data-aos="flip-up" data-aos-easing="linear" data-aos-duration="500"></img>
                     <hr className="hrz it4" data-aos="fade-left" data-aos-easing="linear" data-aos-duration="500" />
                     <div className="text1 it5" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="500">IBSsoftware <br /> <span style={{ color: `var(--highlight)` }}>[ 2024 ]</span></div>
 
@@ -27,7 +23,7 @@ const Experience = () => {
 
                     <div className="text it6" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="500">Full-stack Developer Intern</div>
                     <hr className="hrz it7" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="500" />
-                    <img className="ig it8" src={ISC} data-aos="flip-up" data-aos-easing="linear" data-aos-duration="500"></img>
+                    <img className="ig it8" src={DEV} data-aos="flip-up" data-aos-easing="linear" data-aos-duration="500"></img>
                     <hr className="hrz it9" data-aos="fade-left" data-aos-easing="linear" data-aos-duration="500" />
                     <div className="text1 it10" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="500">Cardiocare & VNG <br /> <span style={{ color: `var(--highlight)` }}>[ Jan 2023 - May 2023 ]</span></div>
 
@@ -35,7 +31,7 @@ const Experience = () => {
 
                     <div className="text it11" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="500">Full-stack Developer Intern</div>
                     <hr className="hrz it12" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="500" />
-                    <img className="ig it13" src={ISC} data-aos="flip-up" data-aos-easing="linear" data-aos-duration="500"></img>
+                    <img className="ig it13" src={DEV} data-aos="flip-up" data-aos-easing="linear" data-aos-duration="500"></img>
                     <hr className="hrz it14" data-aos="fade-left" data-aos-easing="linear" data-aos-duration="500" />
                     <div className="text1 it15" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="500">Celebrare<br /> <span style={{ color: `var(--highlight)` }}>[ Sept 2021 - Oct 2021 ]</span></div>
 
@@ -43,7 +39,7 @@ const Experience = () => {
 
                     <div className="text it16" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="500">Top 74 in HackwithInfy'22</div>
                     <hr className="hrz it17" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="500" />
-                    <img className="ig it18" src={ISC} data-aos="flip-up" data-aos-easing="linear" data-aos-duration="500"></img>
+                    <img className="ig it18" src={DEV} data-aos="flip-up" data-aos-easing="linear" data-aos-duration="500"></img>
                     <hr className="hrz it19" data-aos="fade-left" data-aos-easing="linear" data-aos-duration="500" />
                     <div className="text1 it20" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="500">Infosys<br /> <span style={{ color: `var(--highlight)` }}>Campus Ambassador[ May 2022 - May 2023 ]</span></div>
 
